6deb3ce7bd410ca27fc5dffc1495b9336de9f8e4
Fardhan Ardhi <fa.dsg.id@gmail.com>
2018-08-11 19:08:11 +0700

    draw blocks

---
+++ Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject blockPrefab;
+
+    private GameObject foundationObject;
+
+    private Vector3 blockOffset = new Vector3(1.5f, 1.5f, 1.5f);
+    private Vector3 foundationCenter = new Vector3(2.5f, 0, 2.5f);
+
+	// Use this for initialization
+	void Start () {
+        foundationObject = GameObject.Find("Foundation");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 30.0f))
+            {
+                GameObject go = Instantiate(blockPrefab) as GameObject;
+                go.transform.position = hit.point;
+            }
+        }
+	}
+}
