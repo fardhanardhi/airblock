85f9fcddd8b280edd9b13baa37c01db8b04b75eb
Fardhan Ardhi <fa.dsg.id@gmail.com>
2018-08-16 22:04:44 +0700

    add basic ui

---
+++ Assets/Script/SaveManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour {
+
+    public GameObject saveMenu;
+    public GameObject confirmMenu;
+
+    public Transform saveList;
+    public GameObject savePrefab;
+
+    public void OnSaveMenuClick()
+    {
+        saveMenu.SetActive(true);
+    }
+
+    public void OnSaveClick()
+    {
+        saveMenu.SetActive(false);
+        confirmMenu.SetActive(true);
+    }
+
+    public void OnLoadClick()
+    {
+        saveMenu.SetActive(false);
+        confirmMenu.SetActive(true);
+    }
+
+    public void OnCancelClick()
+    {
+        saveMenu.SetActive(false);
+    }
+
+    public void OnConfirmOk()
+    {
+        //save or load
+        confirmMenu.SetActive(false);
+    }
+
+    public void OnConfirmCancel()
+    {
+        confirmMenu.SetActive(false);
+    }
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
